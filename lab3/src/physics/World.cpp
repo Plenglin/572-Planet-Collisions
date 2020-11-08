88c38e8f51225647fb12c6c826a16cc198db995b
Astrid Yu <astrid@astrid.tech>
2020-11-07 16:54:27 -0800

    Momentum calculation currently NaNing

--- lab3/src/physics/World.cpp
+++ lab3/src/physics/World.cpp
@@ -62,7 +62,7 @@ void World::step(float dt) {
     find_contacts();
     solve_contacts();
 
-    //gravitate(dt);
+    gravitate(dt);
     integrate(dt);
 }
 
@@ -87,10 +87,47 @@ void World::find_contacts() {
 
 void World::solve_contacts() {
     for (auto &c : contacts) {
+        // De-intersect particles
         auto a_deflection = c.normal * c.b->mass / (c.a->mass + c.b->mass);
         auto b_deflection = c.normal - a_deflection;
         c.a->pos += a_deflection;
         c.b->pos -= b_deflection;
+
+        // Initial momentums along normal
+        auto unit_normal = normalize(c.normal);
+        auto pa = dot(unit_normal, c.b->vel);
+        auto pb = dot(unit_normal, c.a->vel);
+        auto pnet = pa + pb;
+
+        // Initial kinetic energy (times 2)
+        auto sa = length(c.a->vel);
+        auto sb = length(c.b->vel);
+        auto e2_initial = c.a->mass * sa * sa + c.b->mass * sb * sb;
+
+        // Energy after applying restitution
+        auto e2_final = e2_initial * 0.8;
+
+        // Quadratic equation parameters to solve for va
+        auto quad_a = c.a->mass * c.a->mass;
+        auto quad_b = c.a->mass * (c.b->mass - 2 * pnet);
+        auto quad_c = pnet * pnet - e2_final * c.b->mass;
+
+        // Perform calculations for final velocities along normal
+        auto discrim = quad_b * quad_b - 4 * quad_a * quad_c;
+        auto sol_offset = sqrt(discrim) / (2 * quad_a);
+        auto sol_center = quad_b / (2 * quad_a);
+        auto va_final = sol_center + sol_offset;
+        auto vb_final = (pnet - pa) / c.b->mass;
+
+        // Apply momentum calculation
+        auto va_orthogonal = c.a->vel - normalize(c.a->vel) * dot(unit_normal, c.a->vel);
+        auto vb_orthogonal = c.b->vel - normalize(c.b->vel) * dot(unit_normal, c.b->vel);
+        auto va_normal = unit_normal;
+        va_normal *= va_final;
+        auto vb_normal = unit_normal;
+        vb_normal *= vb_final;
+        c.a->vel = va_orthogonal + va_normal;
+        c.b->vel = vb_orthogonal + vb_normal;
     }
 }
 
